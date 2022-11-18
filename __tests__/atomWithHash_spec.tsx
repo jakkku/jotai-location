48f62232ba95811c98985989aafa5c11edc29920
sung jin kim <iamsungjinkim@gmail.com>
2022-11-18 23:00:29 +0900

    test: add test for initializing with no hash

--- __tests__/atomWithHash_spec.tsx
+++ __tests__/atomWithHash_spec.tsx
@@ -43,6 +43,41 @@ describe('atomWithHash', () => {
     expect(window.location.hash).toEqual('');
   });
 
+  it('initialize with no hash', async () => {
+    const countAtom = atomWithHash('count', 1);
+
+    const Counter = () => {
+      const [count, setCount] = useAtom(countAtom);
+      return (
+        <>
+          <div>count: {count}</div>
+          <button type="button" onClick={() => setCount((c) => c + 1)}>
+            button
+          </button>
+          <button type="button" onClick={() => setCount(RESET)}>
+            reset
+          </button>
+        </>
+      );
+    };
+
+    const { findByText, getByText } = render(
+      <StrictMode>
+        <Counter />
+      </StrictMode>,
+    );
+
+    await findByText('count: 1');
+
+    fireEvent.click(getByText('button'));
+    await findByText('count: 2');
+    expect(window.location.hash).toEqual('#count=2');
+
+    window.history.back();
+    await findByText('count: 1');
+    expect(window.location.hash).toEqual('');
+  });
+
   it('returning reset from state dispatcher', async () => {
     const isVisibleAtom = atomWithHash('isVisible', true);
 
